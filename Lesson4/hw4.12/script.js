80056631cf6b7118476e5dc960c1597c2b66e7f8
jeydi-art <dijeydi@gmail.com>
2025-11-05 11:47:35 +0100

    done hw4.12

---
+++ Lesson4/hw4.12/script.js
@@ -0,0 +1,22 @@
+
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+
+    // создаю переменную, куда потом запишу найденный объект с нужной валютой
+    let chosenCurrency;
+
+    // если значение поля currency совпадает с валютой, которую нужно найти (exchangeCurrency),
+    // то этот объект сохраняется в chosenCurrency
+    for (const item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            chosenCurrency = item;
+        }
+
+    }
+let result = sumUAH / chosenCurrency.value;
+    return result;
+}
+console.log(exchange(
+    10000,
+    [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}],
+    'USD'));
+
