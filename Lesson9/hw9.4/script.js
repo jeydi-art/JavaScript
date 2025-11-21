6be684a49221886ea257ad55ed0b8fa269ff085b
jeydi-art <dijeydi@gmail.com>
2025-11-21 16:15:59 +0100

    done hw9.4 Lesson9

--- Lesson9/hw9.4/script.js
+++ Lesson9/hw9.4/script.js
@@ -7,11 +7,32 @@ let coursesAndDurationArray = [
     {title: 'Frontend', monthDuration: 4}
 ];
 
+//первый вариант
+// for (const course of coursesAndDurationArray) {
+//     const courseDiv = document.createElement('div');
+//     const h2 = document.createElement('h2');
+//     h2.textContent = course.title;
+//     const p = document.createElement('p');
+//     p.textContent = course.monthDuration;
+//
+//     courseDiv.append(h2, p);
+//
+//     document.body.appendChild(courseDiv);
+// }
+
+//второй вариант
+
 for (const course of coursesAndDurationArray) {
+
     const courseDiv = document.createElement('div');
+    courseDiv.classList.add('item');
+
     const h2 = document.createElement('h2');
+    h2.classList.add('heading');
     h2.textContent = course.title;
+
     const p = document.createElement('p');
+    p.classList.add('description');
     p.textContent = course.monthDuration;
 
     courseDiv.append(h2, p);
