9ec1b51da154c586483fbb9e165e0590186c342f
jeydi-art <dijeydi@gmail.com>
2025-11-21 14:33:02 +0100

    done hw9.1 Lesson9

--- Lesson9/hw9.1/script.js
+++ Lesson9/hw9.1/script.js
@@ -1,7 +1,8 @@
 const block = document.createElement('div');
 block.classList.add('wrap', 'collapse', 'alpha', 'beta');
-document.body.appendChild(block);
 block.textContent = 'Original block';
+
 const clone = block.cloneNode(true);
-document.body.appendChild(clone);
 clone.textContent = 'Cloned block!';
+
+document.body.append(block, clone);
